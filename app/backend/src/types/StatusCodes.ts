932914fa693c76fadb2cc84de6c2a9e020bfecde
rafael moraes <rafaelsm003@gmail.com>
2022-09-28 18:20:53 -0300

    feat: cria enum de status http

---
+++ app/backend/src/types/StatusCodes.ts
@@ -0,0 +1,12 @@
+enum StatusCodes {
+  OK = 200,
+  CREATED = 201,
+  SUCCESS_NO_RESPONSE = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  SEMANTIC_ERROR = 422,
+  SERVER_ERROR = 500,
+}
+
+export default StatusCodes;
