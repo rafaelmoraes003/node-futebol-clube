ae47b724423bab68ccc2fd9539059a4edd4a7b8a
rafael moraes <rafaelsm003@gmail.com>
2022-09-28 14:54:06 -0300

    feat: cria model para a tabela users

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,40 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
