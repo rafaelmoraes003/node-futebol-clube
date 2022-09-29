226fdced4c103fd4dca96513cffae034bea8e361
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 18:33:29 -0300

    feat: cria arquivo para armazena array mockado dos times

---
+++ app/backend/src/tests/utils/teams.ts
@@ -0,0 +1,20 @@
+const teams = [
+  {
+    id: 1,
+    teamName: 'Avaí/Kindermann',
+  },
+  {
+    id: 2,
+    teamName: 'Bahia',
+  },
+  {
+    id: 3,
+    teamName: 'Botafogo',
+  },
+  {
+    id: 4,
+    teamName: 'Corinthians',
+  },
+]
+
+export default teams;
