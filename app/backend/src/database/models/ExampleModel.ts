083ac80763e0db04829ec42739ef75dff5f77255
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 12:45:45 -0300

    comenta arquivo de model de exemplo

--- app/backend/src/database/models/ExampleModel.ts
+++ app/backend/src/database/models/ExampleModel.ts
@@ -1,20 +1,20 @@
-import { Model } from 'sequelize';
-import db from '.';
-// import OtherModel from './OtherModel';
+// import { Model } from 'sequelize';
+// import db from '.';
+// // import OtherModel from './OtherModel';
 
-class Example extends Model {
-  // public <campo>!: <tipo>;
-}
+// class Example extends Model {
+//   // public <campo>!: <tipo>;
+// }
 
-Example.init({
-  // ... Campos
-}, {
-  // ... Outras configs
-  underscored: true,
-  sequelize: db,
-  // modelName: 'example',
-  timestamps: false,
-});
+// Example.init({
+//   // ... Campos
+// }, {
+//   // ... Outras configs
+//   underscored: true,
+//   sequelize: db,
+//   // modelName: 'example',
+//   timestamps: false,
+// });
 
 /**
   * `Workaround` para aplicar as associations em TS:
@@ -27,4 +27,4 @@ Example.init({
 // Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
 // Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
 
-export default Example;
+// export default Example;
