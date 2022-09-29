42ccbb9877a9f004afcae7aa3b66aee77c90ee2f
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 16:53:34 -0300

    test: desenvolve os testes da rota GET /login/validate

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -8,6 +8,9 @@ import User from '../database/models/user';
 
 import { Response } from 'superagent';
 
+import {token, wrongToken } from './utils/token';
+import StatusCodes from '../types/StatusCodes';
+
 chai.use(chaiHttp);
 
 const { expect } = chai;
@@ -41,7 +44,7 @@ describe('Testa a rota POST /login', () => {
           password: 'secret_admin',
         });
 
-      expect(response.status).to.be.equal(200);
+      expect(response.status).to.be.equal(StatusCodes.OK);
       expect(response.body).to.have.key('token');
     });
 
@@ -68,7 +71,7 @@ describe('Testa a rota POST /login', () => {
           password: '1234567',
         });
 
-        expect(response.status).to.be.equal(401);
+        expect(response.status).to.be.equal(StatusCodes.UNAUTHORIZED);
         expect(response.body.message).to.be.equal('Incorrect email or password');
     });
 
@@ -81,7 +84,7 @@ describe('Testa a rota POST /login', () => {
         .request(app)
         .post('/login');
 
-      expect(response.status).to.be.equal(400);
+      expect(response.status).to.be.equal(StatusCodes.BAD_REQUEST);
       expect(response.body).to.have.key('message');
       expect(response.body.message).to.be.equal('All fields must be filled');
     });
@@ -99,7 +102,7 @@ describe('Testa a rota POST /login', () => {
           password: '',
         });
 
-      expect(response.status).to.be.equal(400);
+      expect(response.status).to.be.equal(StatusCodes.BAD_REQUEST);
       expect(response.body).to.have.key('message');
       expect(response.body.message).to.be.equal('All fields must be filled');
     });
@@ -117,7 +120,7 @@ describe('Testa a rota POST /login', () => {
           password: '123456',
         });
 
-      expect(response.status).to.be.equal(422);
+      expect(response.status).to.be.equal(StatusCodes.SEMANTIC_ERROR);
     });
 
   });
@@ -149,9 +152,97 @@ describe('Testa a rota POST /login', () => {
           password: 'senhaerrada',
         });
 
-        expect(response.status).to.be.equal(401);
+        expect(response.status).to.be.equal(StatusCodes.UNAUTHORIZED);
         expect(response.body.message).to.be.equal('Unauthorized user');
     });
 
   });
+});
+
+describe('Testa a rota GET /login/validate', () => {
+
+  describe('Testa em caso de sucesso', () => {
+
+    before(async () => {
+      sinon
+        .stub(User, 'findOne')
+          .resolves({
+            id: 1,
+            username: 'Admin',
+            role: 'admin',
+            email: 'admin@admin.com',
+            password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+          } as User);
+    });
+
+    after(async () => {
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Verifica se retorna o cargo do usuário com status 200', async () => {
+      const response: Response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set({ 'authorization': token });
+
+      expect(response.status).to.be.equal(StatusCodes.OK);
+      expect(response.body).to.have.key('role');
+      expect(response.body.role).to.be.equal('admin');
+    });
+
+  });
+
+  describe('Testa em caso de falha no servidor', () => {
+
+    before(async () => {
+      sinon
+        .stub(User, 'findOne')
+        .rejects();
+    });
+
+    after(async () => {
+      (User.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Verifica se retorna erro de servidor 500', async () => {
+
+      const response: Response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set({ 'authorization': token });
+
+      expect(response.status).to.be.equal(StatusCodes.SERVER_ERROR);
+    });
+
+  });
+
+  describe('Testa em caso que o token é undefined', () => {
+
+    it('Verifica se retorna uma mensagem com status 404', async () => {
+
+      const response: Response = await chai
+        .request(app)
+        .get('/login/validate')
+
+      expect(response.status).to.be.equal(StatusCodes.NOT_FOUND);
+      expect(response.body.message).to.be.equal('Token not found.');
+    });
+
+  });
+
+  describe('Testa em caso de assinatura do token jwt inválida', () => {
+
+    it('Verifica se retorna uma mensagem com status 400', async () => {
+
+      const response: Response = await chai
+        .request(app)
+        .get('/login/validate')
+        .set('authorization', wrongToken );
+
+      expect(response.status).to.be.equal(StatusCodes.BAD_REQUEST);
+      expect(response.body.message).to.be.equal('Invalid signature.');
+    });
+
+  });
+
 });
