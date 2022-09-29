6e50e44dabbb2de8a4d708c1e43842ab56e54ed5
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 19:54:45 -0300

    feat: cria método na classe TeamsService para listar time baseado em seu id

--- app/backend/src/services/teams.ts
+++ app/backend/src/services/teams.ts
@@ -9,4 +9,10 @@ export default class TeamsService {
     const teams = await this._teamsModel.findAll();
     return { code: StatusCodes.OK, data: teams };
   }
+
+  public async getById(id: number): Promise<TeamsServiceResponse<Team>> {
+    const team = await this._teamsModel.findOne({ where: { id } });
+    if (!team) return { code: StatusCodes.NOT_FOUND, error: 'Not found.' };
+    return { code: StatusCodes.OK, data: team };
+  }
 }
