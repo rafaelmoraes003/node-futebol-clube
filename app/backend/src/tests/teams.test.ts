978ecfc4ff85a43a8ff9f759951176a3c557559b
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 19:55:13 -0300

    test: desenvolve testes para o endpoint GET /teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -63,3 +63,76 @@ describe('Testa a rota GET /teams', () => {
   });
 
 });
+
+describe('Testa o endpoint GET /teams/:id', () => {
+
+  describe('Testa em caso de sucesso', () => {
+
+    before(async () => {
+      sinon
+        .stub(Team, 'findOne')
+        .resolves(teams[0] as Team);
+    });
+
+    after(async () => {
+      (Team.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Verifica se retorna 1 time com status 200', async () => {
+      const response: Response = await chai
+        .request(app)
+        .get('/teams/1');
+
+      expect(response.status).to.be.equal(StatusCodes.OK);
+      expect(response.body).to.have.keys('id', 'teamName');
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.deep.include(teams[0]);
+    });
+
+  });
+
+  describe('Testa em caso de time inexistente', () => {
+
+    before(async () => {
+      sinon
+        .stub(Team, 'findOne')
+        .resolves(null);
+    });
+
+    after(async () => {
+      (Team.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Verifica se retorna erro com status 404', async () => {
+      const response: Response = await chai
+        .request(app)
+        .get('/teams/999');
+
+      expect(response.status).to.be.equal(StatusCodes.NOT_FOUND);
+    });
+
+  });
+
+  describe('Testa em caso de erro no servidor', () => {
+
+    before(async () => {
+      sinon
+        .stub(Team, 'findOne')
+        .rejects();
+    });
+
+    after(async () => {
+      (Team.findOne as sinon.SinonStub).restore();
+    });
+
+    it('Verifica se retorna erro com status 500', async () => {
+      const response: Response = await chai
+        .request(app)
+        .get('/teams/5');
+
+      expect(response.status).to.be.equal(StatusCodes.SERVER_ERROR);
+    });
+
+  });
+
+});
