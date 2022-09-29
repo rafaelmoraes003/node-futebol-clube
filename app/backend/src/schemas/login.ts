841ecd995aef005214e297593a7dd804e607f34c
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 12:47:54 -0300

    feat: adiciona mensagem de erro personalizada para error do corpo da requisição de login

--- app/backend/src/schemas/login.ts
+++ app/backend/src/schemas/login.ts
@@ -1,13 +1,23 @@
 import * as joi from 'joi';
 
+const errorMessage = 'All fields must be filled';
+
 const loginSchema = joi.object({
   email: joi
     .string()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .messages({
+      'any.required': errorMessage,
+      'string.empty': errorMessage,
+    })
     .required(),
   password: joi
     .string()
     .min(7)
+    .messages({
+      'any.required': errorMessage,
+      'string.empty': errorMessage,
+    })
     .required(),
 });
 
