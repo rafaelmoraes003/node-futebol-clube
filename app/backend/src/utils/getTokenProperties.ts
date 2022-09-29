5a37570edb73cd9b3bc9033527f3c476a080c258
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 16:54:54 -0300

    refactor: modifica a maneira que o token jwt é verificado

--- app/backend/src/utils/getTokenProperties.ts
+++ app/backend/src/utils/getTokenProperties.ts
@@ -1,11 +1,14 @@
 import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
 import CustomError from '../types/CustomError';
 import StatusCodes from '../types/StatusCodes';
 import { ITokenPayload } from './getToken';
 
+dotenv.config();
+
 const getTokenProperties = (token: string): number => {
   try {
-    const { id } = jwt.verify(token, String(process.env.JWT_SECRET)) as ITokenPayload;
+    const { id } = jwt.verify(token, process.env.JWT_SECRET as string) as ITokenPayload;
     return id;
   } catch (error) {
     throw new CustomError(StatusCodes.BAD_REQUEST, 'Invalid signature.');
