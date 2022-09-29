9f5557668e50c47e40a431f0ec692aadb27c4c9c
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 17:36:57 -0300

    feat: cria migration para a tabela teams

---
+++ app/backend/src/database/migrations/02-create-team-table.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      team_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+  down: async(queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  },
+};
