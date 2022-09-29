dce7f53b55eb33bd939a2273faffab2b6e913236
rafael moraes <rafaelsm003@gmail.com>
2022-09-29 17:36:43 -0300

    feat: cria model para a tabela teams

---
+++ app/backend/src/database/models/team.ts
@@ -0,0 +1,26 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Team;
