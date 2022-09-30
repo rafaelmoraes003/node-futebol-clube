a2fa28e61a89d0ab17de9270fbfb272c4b60f1fe
rafael moraes <rafaelsm003@gmail.com>
2022-09-30 20:38:54 -0300

    feat: cria o endpoint POST /matches

--- app/backend/src/routes/matches.ts
+++ app/backend/src/routes/matches.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import auth from '../middlewares/auth';
 import MatchesController from '../controllers/matches';
 
 const matches = Router();
@@ -6,5 +7,7 @@ const matches = Router();
 const matchesController = new MatchesController();
 
 matches.get('/', matchesController.getAll);
+
+matches.post('/', auth, matchesController.create);
 
 export default matches;
