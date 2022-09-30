5202ef311619f9456927863bcca56cb724cb41dd
rafael moraes <rafaelsm003@gmail.com>
2022-09-30 15:41:32 -0300

    feat: cria a model da tabela matches

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,51 @@
+import { Model, DataTypes } from 'sequelize';
+import Team from './team';
+import db from '.';
+
+class Match extends Model {
+  id!: number;
+  homeTeam!: number;
+  homeTeamGoals!: number;
+  awayTeam!: number;
+  inProgress!: number;
+}
+
+Match.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Match.belongsTo(Team, {
+  foreignKey: 'id' as 'homeTeam',
+});
+
+Match.belongsTo(Team, {
+  foreignKey: 'id' as 'awayTeam',
+});
