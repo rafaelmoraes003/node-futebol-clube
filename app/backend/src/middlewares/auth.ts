5e8c8f694def40e978b76a272a125008cd195f6e
rafael moraes <rafaelsm003@gmail.com>
2022-09-30 20:32:16 -0300

    feat: cria middleware de autenticação

---
+++ app/backend/src/middlewares/auth.ts
@@ -0,0 +1,25 @@
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+import { Request, Response, NextFunction } from 'express';
+import StatusCodes from '../types/StatusCodes';
+
+dotenv.config();
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  const { authorization: token } = req.headers;
+  if (!token) {
+    return res.status(StatusCodes.NOT_FOUND).json({
+      message: 'Token not found',
+    });
+  }
+  try {
+    jwt.verify(token, String(process.env.JWT_SECRET));
+    next();
+  } catch (error) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'Token must be a valid token',
+    });
+  }
+};
+
+export default auth;
