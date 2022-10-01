6bac65140ef3374383b1021224d4eb40ffd0a852
rafael moraes <rafaelsm003@gmail.com>
2022-10-01 15:02:10 -0300

    refactor: modifica a função que faz validação de times

--- app/backend/src/utils/validateTeams.ts
+++ app/backend/src/utils/validateTeams.ts
@@ -3,8 +3,8 @@ import StatusCodes from '../types/StatusCodes';
 import Team from '../database/models/team';
 
 const validateTeams = async (teams: number[]): Promise<void> => {
-  const count = await Team.findAll({ where: { id: teams } });
-  if (count.length !== teams.length) {
+  const { count } = await Team.findAndCountAll({ where: { id: teams } });
+  if (count !== teams.length) {
     throw new CustomError(
       StatusCodes.NOT_FOUND,
       'There is no team with such id!',
