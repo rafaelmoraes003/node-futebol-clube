6bf8194059fbe4bd195292053bec557fbaaa136b
rafael moraes <rafaelsm003@gmail.com>
2022-10-03 16:17:22 -0300

    feat: cria método na classe MatchController para atualização de resultados de partidas

--- app/backend/src/controllers/matches.ts
+++ app/backend/src/controllers/matches.ts
@@ -41,4 +41,19 @@ export default class MatchesController {
       next(error);
     }
   };
+
+  public updateMatch = async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const { homeTeamGoals, awayTeamGoals } = req.body;
+    try {
+      const { code, data, error } = await this._matchesService.updateMatch(
+        Number(id),
+        { homeTeamGoals, awayTeamGoals },
+      );
+      if (error) return res.status(code).json({ message: error });
+      return res.status(code).json({ message: data });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
