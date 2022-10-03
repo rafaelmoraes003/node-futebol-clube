f04b997b696df8072ec0e5c7806e15dacb820947
rafael moraes <rafaelsm003@gmail.com>
2022-10-03 14:47:42 -0300

    feat: extende função que valida times ao não possibilitar criação de partidas com times iguais

--- app/backend/src/utils/validateTeams.ts
+++ app/backend/src/utils/validateTeams.ts
@@ -3,6 +3,13 @@ import StatusCodes from '../types/StatusCodes';
 import Team from '../database/models/team';
 
 const validateTeams = async (teams: number[]): Promise<void> => {
+  if (teams[0] === teams[1]) {
+    throw new CustomError(
+      StatusCodes.UNAUTHORIZED,
+      'It is not possible to create a match with two equal teams',
+    );
+  }
+
   const { count } = await Team.findAndCountAll({ where: { id: teams } });
   if (count !== teams.length) {
     throw new CustomError(
