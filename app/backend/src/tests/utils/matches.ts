23cddf3d01fe5d0d4e3140c4da71719c12b7bc02
rafael moraes <rafaelsm003@gmail.com>
2022-10-04 15:15:40 -0300

    feat: cria arrays mockados de partidas em andamento e partidas finalizadas

--- app/backend/src/tests/utils/matches.ts
+++ app/backend/src/tests/utils/matches.ts
@@ -1,30 +1,16 @@
-const matches = [
+export const matchesNotInProgress = [
 	{
 		id: 1,
 		homeTeam: 16,
 		homeTeamGoals: 1,
 		awayTeam: 8,
 		awayTeamGoals: 1,
-		inProgress: true,
+		inProgress: false,
 		teamHome: {
-			teamName: 'São Paulo'
+			teamName: "São Paulo"
 		},
 		teamAway: {
-			teamName: 'Grêmio'
-		}
-	},
-  {
-		id: 42,
-		homeTeam: 6,
-		homeTeamGoals: 1,
-		awayTeam: 1,
-		awayTeamGoals: 0,
-		inProgress: true,
-		teamHome: {
-			teamName: 'Ferroviária'
-		},
-		teamAway: {
-			teamName: 'Avaí/Kindermann'
+			teamName: "Grêmio"
 		}
 	},
 	{
@@ -35,13 +21,13 @@ const matches = [
 		awayTeamGoals: 1,
 		inProgress: false,
 		teamHome: {
-			teamName: 'Internacional',
+			teamName: "Internacional"
 		},
 		teamAway: {
-			teamName: 'Santos',
+			teamName: "Santos"
 		}
 	},
-  {
+	{
 		id: 3,
 		homeTeam: 4,
 		homeTeamGoals: 3,
@@ -49,12 +35,195 @@ const matches = [
 		awayTeamGoals: 0,
 		inProgress: false,
 		teamHome: {
-			teamName: 'Corinthians'
+			teamName: "Corinthians"
 		},
 		teamAway: {
-			teamName: 'Napoli-SC'
+			teamName: "Napoli-SC"
 		}
 	},
-];
+	{
+		id: 4,
+		homeTeam: 3,
+		homeTeamGoals: 0,
+		awayTeam: 2,
+		awayTeamGoals: 0,
+		inProgress: false,
+		teamHome: {
+			teamName: "Botafogo"
+		},
+		teamAway: {
+			teamName: "Bahia"
+		}
+	},
+	{
+		id: 5,
+		homeTeam: 7,
+		homeTeamGoals: 1,
+		awayTeam: 10,
+		awayTeamGoals: 1,
+		inProgress: false,
+		teamHome: {
+			teamName: "Flamengo"
+		},
+		teamAway: {
+			teamName: "Minas Brasília"
+		}
+	},
+	{
+		id: 6,
+		homeTeam: 5,
+		homeTeamGoals: 1,
+		awayTeam: 13,
+		awayTeamGoals: 1,
+		inProgress: false,
+		teamHome: {
+			teamName: "Cruzeiro"
+		},
+		teamAway: {
+			teamName: "Real Brasília"
+		}
+	},
+	{
+		id: 7,
+		homeTeam: 12,
+		homeTeamGoals: 2,
+		awayTeam: 6,
+		awayTeamGoals: 2,
+		inProgress: false,
+		teamHome: {
+			teamName: "Palmeiras"
+		},
+		teamAway: {
+			teamName: "Ferroviária"
+		}
+	},
+	{
+		id: 8,
+		homeTeam: 15,
+		homeTeamGoals: 0,
+		awayTeam: 1,
+		awayTeamGoals: 1,
+		inProgress: false,
+		teamHome: {
+			teamName: "São José-SP"
+		},
+		teamAway: {
+			teamName: "Avaí/Kindermann"
+		}
+	},
+]
 
-export default matches;
+export const matchesInProgress = [
+	{
+		id: 41,
+		homeTeam: 16,
+		homeTeamGoals: 2,
+		awayTeam: 9,
+		awayTeamGoals: 0,
+		inProgress: true,
+		teamHome: {
+			teamName: "São Paulo"
+		},
+		teamAway: {
+			teamName: "Internacional"
+		}
+	},
+	{
+		id: 42,
+		homeTeam: 6,
+		homeTeamGoals: 1,
+		awayTeam: 1,
+		awayTeamGoals: 0,
+		inProgress: true,
+		teamHome: {
+			teamName: "Ferroviária"
+		},
+		teamAway: {
+			teamName: "Avaí/Kindermann"
+		}
+	},
+	{
+		id: 43,
+		homeTeam: 11,
+		homeTeamGoals: 0,
+		awayTeam: 10,
+		awayTeamGoals: 0,
+		inProgress: true,
+		teamHome: {
+			teamName: "Napoli-SC"
+		},
+		teamAway: {
+			teamName: "Minas Brasília"
+		}
+	},
+	{
+		id: 44,
+		homeTeam: 7,
+		homeTeamGoals: 2,
+		awayTeam: 15,
+		awayTeamGoals: 2,
+		inProgress: true,
+		teamHome: {
+			teamName: "Flamengo"
+		},
+		teamAway: {
+			teamName: "São José-SP"
+		}
+	},
+	{
+		id: 45,
+		homeTeam: 5,
+		homeTeamGoals: 1,
+		awayTeam: 3,
+		awayTeamGoals: 1,
+		inProgress: true,
+		teamHome: {
+			teamName: "Cruzeiro"
+		},
+		teamAway: {
+			teamName: "Botafogo"
+		}
+	},
+	{
+		id: 46,
+		homeTeam: 4,
+		homeTeamGoals: 1,
+		awayTeam: 12,
+		awayTeamGoals: 1,
+		inProgress: true,
+		teamHome: {
+			teamName: "Corinthians"
+		},
+		teamAway: {
+			teamName: "Palmeiras"
+		}
+	},
+	{
+		id: 47,
+		homeTeam: 8,
+		homeTeamGoals: 1,
+		awayTeam: 14,
+		awayTeamGoals: 2,
+		inProgress: true,
+		teamHome: {
+			teamName: "Grêmio"
+		},
+		teamAway: {
+			teamName: "Santos"
+		}
+	},
+	{
+		id: 48,
+		homeTeam: 13,
+		homeTeamGoals: 1,
+		awayTeam: 2,
+		awayTeamGoals: 1,
+		inProgress: true,
+		teamHome: {
+			teamName: "Real Brasília"
+		},
+		teamAway: {
+			teamName: "Bahia"
+		}
+	}
+];
