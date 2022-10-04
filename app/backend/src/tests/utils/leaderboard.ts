2dd0eeefdbb8224f14e0e3ec7b4e633e360b31ce
rafael moraes <rafaelsm003@gmail.com>
2022-10-04 17:22:08 -0300

    feat: cria array mockado de placar de desempenho dos times em todas as partidas jogadas

--- app/backend/src/tests/utils/leaderboard.ts
+++ app/backend/src/tests/utils/leaderboard.ts
@@ -387,3 +387,198 @@ export const awayLeaderboard = [
     efficiency: '0.00'
   }
 ];
+
+export const homeAndAwayLeaderboard = [
+  {
+    name: 'Corinthians',
+    totalPoints: 3,
+    totalGames: 1,
+    totalVictories: 1,
+    totalDraws: 0,
+    totalLosses: 0,
+    goalsFavor: 3,
+    goalsOwn: 0,
+    goalsBalance: 3,
+    efficiency: '100.00'
+  },
+  {
+    name: 'Avaí/Kindermann',
+    totalPoints: 3,
+    totalGames: 1,
+    totalVictories: 1,
+    totalDraws: 0,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 0,
+    goalsBalance: 1,
+    efficiency: '100.00'
+  },
+  {
+    name: 'Ferroviária',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 2,
+    goalsOwn: 2,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Palmeiras',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 2,
+    goalsOwn: 2,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Cruzeiro',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Flamengo',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Grêmio',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Internacional',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Minas Brasília',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Real Brasília',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Santos',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'São Paulo',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 1,
+    goalsOwn: 1,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Bahia',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 0,
+    goalsOwn: 0,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'Botafogo',
+    totalPoints: 1,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 1,
+    totalLosses: 0,
+    goalsFavor: 0,
+    goalsOwn: 0,
+    goalsBalance: 0,
+    efficiency: '33.33'
+  },
+  {
+    name: 'São José-SP',
+    totalPoints: 0,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 0,
+    totalLosses: 1,
+    goalsFavor: 0,
+    goalsOwn: 1,
+    goalsBalance: -1,
+    efficiency: '0.00'
+  },
+  {
+    name: 'Napoli-SC',
+    totalPoints: 0,
+    totalGames: 1,
+    totalVictories: 0,
+    totalDraws: 0,
+    totalLosses: 1,
+    goalsFavor: 0,
+    goalsOwn: 3,
+    goalsBalance: -3,
+    efficiency: '0.00'
+  }
+]
