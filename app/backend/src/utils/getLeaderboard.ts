1de40e34b753e7006440885796f4813e41e14a47
rafael moraes <rafaelsm003@gmail.com>
2022-10-04 17:24:39 -0300

    refactor: refatora função getLeaderboard para ser capaz de buscar dados dos times em todos os tipos de partida

--- app/backend/src/utils/getLeaderboard.ts
+++ app/backend/src/utils/getLeaderboard.ts
@@ -1,37 +1,23 @@
 import Match from '../database/models/match';
 import Team from '../database/models/team';
 import { ITeamStats } from '../types/interfaces';
-import { stats, cleanStats, getStats } from './getStats';
+import getDataFromAllMatches from './getDataFromAllMatches';
+import getDataFromHomeOrAwayMatches from './getDataFromHomeOrAwayMatches';
+import { stats, cleanStats } from './getStats';
 import sortStats from './sortStats';
 
-const getLeaderboardStats = (
+const getLeaderboard = (
   teams: Team[],
   matches: Match[],
   type: string,
 ) => teams.map(({ id, teamName }): ITeamStats => {
   cleanStats();
   matches.forEach(({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals }) => {
-    const teamType = type === 'home' ? homeTeam : awayTeam;
-    if (id === teamType) {
-      stats.totalGames += 1;
-      getStats(homeTeamGoals, awayTeamGoals, type);
-    }
-  });
-  return { name: teamName, ...stats };
-});
-
-const getAllLeaderboardStats = (
-  teams: Team[],
-  matches: Match[],
-) => teams.map(({ id, teamName }): ITeamStats => {
-  cleanStats();
-  matches.forEach(({ homeTeam, awayTeam, homeTeamGoals, awayTeamGoals }) => {
-    if (id === homeTeam) {
-      stats.totalGames += 1;
-      getStats(homeTeamGoals, awayTeamGoals, 'home');
-    } else if (id === awayTeam) {
-      stats.totalGames += 1;
-      getStats(homeTeamGoals, awayTeamGoals, 'away');
+    if (type === 'all') {
+      getDataFromAllMatches({ id, homeTeam, awayTeam, homeTeamGoals, awayTeamGoals });
+    } else {
+      const teamType = type === 'home' ? homeTeam : awayTeam;
+      getDataFromHomeOrAwayMatches({ teamType, type, id, homeTeamGoals, awayTeamGoals });
     }
   });
   return { name: teamName, ...stats };
@@ -40,12 +26,9 @@ const getAllLeaderboardStats = (
 const orderedLeaderboard = (
   teams: Team[],
   matches: Match[],
-  type: string | undefined,
+  type: string,
 ): ITeamStats[] => {
-  const leaderboard = type
-    ? getLeaderboardStats(teams, matches, type)
-    : getAllLeaderboardStats(teams, matches);
-
+  const leaderboard = getLeaderboard(teams, matches, type);
   return leaderboard.sort(sortStats);
 };
 
