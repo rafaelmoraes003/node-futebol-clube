ad4876079c3792dd7211e0f128d5e5b3544bbb97
rafael moraes <rafaelsm003@gmail.com>
2022-10-04 15:11:36 -0300

    refactor: método getLeaderboard tem seu retorno tipado e passa a enviar um objeto com placar de times como resposta

--- app/backend/src/services/leaderboard.ts
+++ app/backend/src/services/leaderboard.ts
@@ -1,18 +1,22 @@
 import Team from '../database/models/team';
 import Match from '../database/models/match';
 import orderedLeaderboard from '../utils/getLeaderboard';
+import StatusCodes from '../types/StatusCodes';
+import { ServiceResponse } from '../types/ServiceResponse';
+import { ITeamStats } from '../types/interfaces';
 
 export default class LeaderboardService {
   private _matchModel = Match;
   private _teamModel = Team;
 
-  public async getLeaderboard(type: string) {
+  public async getLeaderboard(type: string): Promise<ServiceResponse<ITeamStats[]>> {
     const teams = await this._teamModel.findAll();
 
     const matches = await this._matchModel.findAll({
       where: { inProgress: false },
     });
 
-    return orderedLeaderboard(teams, matches, type);
+    const leaderboard = orderedLeaderboard(teams, matches, type);
+    return { code: StatusCodes.OK, data: leaderboard };
   }
 }
